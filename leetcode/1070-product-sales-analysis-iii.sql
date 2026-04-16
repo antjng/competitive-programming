28013c6325fd3639a3f34c407bce35f32654199f
Anita Jiang <anitajiang@protonmail.com>
2026-04-16 12:36:14 -0400

    2026-04-16

---
+++ leetcode/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT product_id, year AS first_year, quantity, price
+FROM sales WHERE (product_id, year) in (
+    SELECT product_id, min(year)
+    FROM sales
+    GROUP BY product_id
+)
